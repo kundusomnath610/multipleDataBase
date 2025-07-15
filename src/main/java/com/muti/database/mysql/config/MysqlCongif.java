c8066afabff593cd940a834f1b90e6c8e77c4a56
Somnath Kundu <kundusomnath610@gmail.com>
2025-07-16 01:20:57 +0530

    commit 16th july

--- src/main/java/com/muti/database/mysql/config/MysqlCongif.java
+++ src/main/java/com/muti/database/mysql/config/MysqlCongif.java
@@ -1,7 +1,18 @@
 package com.muti.database.mysql.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+    entityManagerFactoryRef = "",
+    basePackages = {},
+    transactionManagerRef = {}
+)
 public class MysqlCongif {
 
-    
+
 
 }
